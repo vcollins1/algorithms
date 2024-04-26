fbd55bf29f5d829b0e6dce931077d1e15158b134
vcollins1 <vontrellecollins@gmail.com>
2024-04-26 11:55:21 -0400

    evaluate to postfix expression
    can only except expressions with signal digit numbers.

--- infix-to-postfix/main.c
+++ infix-to-postfix/main.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 #include "../stack/include/stack.h"
 #include <string.h>
+#include <stdlib.h>
 
 void infix_to_postfix(char*, char*);
+int evaluate_postfix(char*);
 
 int main(int argc, char* argv[])
 {
-    char* expression = "(a + b / c * (d + c) - f)";
+    char* expression = "(2*(3+4)*5)";
     char postfix[strlen(expression + 1)];
     infix_to_postfix(expression, postfix);
-
     printf("postfix expression: %s\n", postfix);
 
+    int result = evaluate_postfix(postfix);
+    printf("%d\n", result);
     return 0;
 }
 
@@ -67,4 +70,55 @@ void infix_to_postfix(char* infix, char* buffer)
     }
 
     buffer[count] = '\0';
+}
+
+int evaluate_postfix(char* postfix)
+{
+  // can  only evaluate expressions with single digit numbers
+  
+  Stack* stack = new_stack();
+  const int max_expression = strlen(postfix);
+  int expresstion_buffer[max_expression];
+  /* expression index */
+  int express = 0;
+
+  /* index of current postfix char */
+  int i;
+  for (i = 0; i < strlen(postfix); ++i) {
+    if (postfix[i] >= '0' && postfix[i] <= '9') {
+      expresstion_buffer[express] = postfix[i] - '0';
+      stack->push(stack, &expresstion_buffer[express]);
+      ++express;
+    } else {
+      int result;
+      int op2 = *(int*) stack->pop(stack);
+      --express;
+      int op1 = *(int*) stack->pop(stack);
+      --express;
+
+      switch (postfix[i])
+      {
+      case '+':
+        result = op1 + op2;
+        break;
+      case '-':
+        result = op1 - op2;
+        break;
+      case '*':
+        result = op1 * op2;
+        break;
+      case '/':
+        result = op1 / op2;
+        break;    
+      default:
+        continue;
+      }
+
+      expresstion_buffer[express] = result;
+      stack->push(stack, &expresstion_buffer[express++]);
+    }
+  }
+
+
+  return *(int*) stack->pop(stack);
 }
